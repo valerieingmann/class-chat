cbfbb32a3a851cc5da7809354d76fe24fd99b824
Valerie Ingmann <valerieingmann707@gmail.com>
2020-10-01 16:52:25 -0400

    admin can create chat

--- src/components/Admin/index.js
+++ src/components/Admin/index.js
@@ -1,23 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import { compose } from 'recompose';
-
+import { AuthUserContext } from '../Session';
 import { withAuthorization, withEmailVerification } from '../Session';
 import { UserList, UserItem } from '../Users';
 import * as ROLES from '../../constants/roles';
 import * as ROUTES from '../../constants/routes';
+import { withFirebase } from '../Firebase';
 
-const AdminPage = () => (
-  <div>
-    <h1>Admin</h1>
-    <p>The Admin Page is accessible by every signed in admin user.</p>
+class AdminPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+    };
+  }
 
-    <Switch>
-      <Route exact path={ROUTES.ADMIN_DETAILS} component={UserItem} />
-      <Route exact path={ROUTES.ADMIN} component={UserList} />
-    </Switch>
-  </div>
-);
+  onChange = event => {
+    this.setState({ title: event.target.value });
+  };
+  createChat = (event, authUser) => {
+    console.log(authUser);
+    this.props.firebase
+      .chats()
+      .push({ title: this.state.title, ownerId: authUser.uid });
+  };
+
+  render() {
+    return (
+      <AuthUserContext.Consumer>
+        {authUser => (
+          <div>
+            <h1>Admin</h1>
+            <p>
+              The Admin Page is accessible by every signed in admin
+              user.
+            </p>
+            {/* <button
+              onClick={event => this.createChat(event, authUser)}
+            >
+              Create Chat
+            </button> */}
+            <form
+              onSubmit={event => this.createChat(event, authUser)}
+            >
+              <input
+                type="text"
+                value={this.state.title}
+                onChange={this.onChange}
+              />
+              <button type="submit">Create New Chat Room</button>
+            </form>
+
+            <Switch>
+              <Route
+                exact
+                path={ROUTES.ADMIN_DETAILS}
+                component={UserItem}
+              />
+              <Route exact path={ROUTES.ADMIN} component={UserList} />
+            </Switch>
+          </div>
+        )}
+      </AuthUserContext.Consumer>
+    );
+  }
+}
 
 const condition = authUser =>
   authUser && !!authUser.roles[ROLES.ADMIN];
@@ -25,4 +73,5 @@ const condition = authUser =>
 export default compose(
   withEmailVerification,
   withAuthorization(condition),
+  withFirebase,
 )(AdminPage);
--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -3,12 +3,13 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: 'AIzaSyCgsJBAUiXFqi2NUAtJopNOxpxpsPD3FxU',
+  authDomain: 'chat3-c1354.firebaseapp.com',
+  databaseURL: 'https://chat3-c1354.firebaseio.com',
+  projectId: 'chat3-c1354',
+  storageBucket: 'chat3-c1354.appspot.com',
+  messagingSenderId: '463717831647',
+  appId: '1:463717831647:web:da559b8a39b161eaa16a6a',
 };
 
 class Firebase {
@@ -55,7 +56,7 @@ class Firebase {
 
   doSendEmailVerification = () =>
     this.auth.currentUser.sendEmailVerification({
-      url: process.env.REACT_APP_CONFIRMATION_EMAIL_REDIRECT,
+      url: 'http://localhost:3000',
     });
 
   doPasswordUpdate = password =>
@@ -97,6 +98,8 @@ class Firebase {
   user = uid => this.db.ref(`users/${uid}`);
 
   users = () => this.db.ref('users');
+
+  chats = () => this.db.ref('chats');
 
   // *** Message API ***
 
