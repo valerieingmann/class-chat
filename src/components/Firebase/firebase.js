2675bf8701bac55fad5fd233de2c2d1eea9b51ab
Valerie Ingmann <valerieingmann707@gmail.com>
2020-11-10 16:19:11 -0500

    fix: secrets.js

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,7 +1,7 @@
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
-import config from './config';
+import config from './secrets';
 
 class Firebase {
   constructor() {
